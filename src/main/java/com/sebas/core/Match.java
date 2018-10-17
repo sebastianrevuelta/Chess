721870f48a1e3d7be500ebbaeb60629455365719
Sebastián Revuelta <sebastian.revuelta@kiuwan.com>
2018-10-17 10:55:12 +0200

    fix efficiency defect

--- src/main/java/com/sebas/core/Match.java
+++ src/main/java/com/sebas/core/Match.java
@@ -7,72 +7,74 @@ import org.apache.log4j.Logger;
  *
  */
 public class Match {
-	
+
 	private static final long TIME_OUT_THINKING = 2000;
 	private final static Logger log = Logger.getLogger(Match.class);
-	
+
 	private Board board;
 	private String turn;
 	private boolean checkmate;
 	private int movement;
 	private String historyMatch;
-	
+
 	public Board getBoard() { return board; }
 	public void setBoard(Board board) { this.board = board; }
 
 	public String getTurno() { return turn; }
 	public void setTurno(String turno) { this.turn = turno; }
-	
+
 	public String getHistoryMatch() { return historyMatch; }
 	public void setHistoryMatch(String historyMatch) { this.historyMatch = historyMatch; }
-	
+
 	public Match(String turno) {
 		setBoard(new Board());
 		setTurno(turno);
 		movement = 1;
 		setHistoryMatch("");
 	}
-	
+
 	/**
 	 * start the game
 	 */
 	public void startGame() {
 
 		Board board = new Board();
-		
-		while (!checkmate && movement != 6) {
-		  board.print();
-		  Movement m = new Movement();
-		  boolean checkMate = false;
-		  while (!checkMate) {
-			  m = m.makeMovement(board,turn);
-			  checkMate = board.checkMovement(m);
-		  }
-		  board.update(m,turn);
-		  
-		  if ("white".equals(turn)) setTurno("black");
-		  else setTurno("white");
-		  
-		  try { Thread.sleep(TIME_OUT_THINKING); } 
-		  catch (InterruptedException e) { 
-			  log.error("Error in the thread of the match");
-		  }
+		try { 
+			while (!checkmate && movement != 6) {
+				board.print();
+				Movement m = new Movement();
+				boolean checkMate = false;
+				while (!checkMate) {
+					m = m.makeMovement(board,turn);
+					checkMate = board.checkMovement(m);
+				}
+				board.update(m,turn);
+
+				if ("white".equals(turn)) setTurno("black");
+				else setTurno("white");
+
+				Thread.sleep(TIME_OUT_THINKING);  
+
+			}
+		}
+		catch (InterruptedException e) { 
+			log.error("Error in the thread of the match");
 		}
 	}
-	
+
 	/**
 	 * move!
 	 * @return
 	 */
 	public String getMove() {
-		
+
 		Movement m = new Movement();
 		m = m.makeMovement(board,turn);
 		String descriptionMove;
-		
+
 		//boolean checkMate = board.checkMovement(m);
 		board.update(m,turn);
-		
+
 		if ("white".equals(turn)) { 
 			setTurno("black"); 
 			if ("pawn".equals(m.getPiece())) {
@@ -94,8 +96,8 @@ public class Match {
 			setHistoryMatch(descriptionMove);
 			movement++;
 		}
-		
+
 		return getHistoryMatch();
 	}
-	
+
 }
