fda3f776366bf5e527bcfa7634a050e35b1e921e
unknown <sebastianrevuelta@gmail.com>
2018-11-13 12:25:48 +0100

    TuningMovement

--- js/greeter.ts
+++ js/greeter.ts
@@ -1,10 +1,65 @@
-function greeter(person) {
-    return "Hello, " + person;
+function paintSquare(document,square,hasImage,pathImage) {
+
+    var element = document.createElement("div");
+    element.setAttribute("id", square);
+    
+    if (hasImage) {
+        var myloc = new Image();
+        myloc.useMap = pathImage;
+        var img = document.createElement('img')
+        img.setAttribute('src', myloc.useMap);
+        element.appendChild(img);
+    }
+    document.body.appendChild(element);
 }
 
-let user = "Sebas";
+document.title = "VulnChess";
+let vertical = ["1","2","3","4","5","6","7","8"];
+let horizontal = ["a","b","c","d","e","f","g","h"];
 
-let match = Java.type("com.sebas.core.Match");
+for (let i of horizontal) {
+    for (let j of vertical) {
+        let square:string = i+j;
+        let color:string = "w";
 
-document.title = "VulnChess"
-document.body.innerHTML = greeter(user);
+        //Choose color
+        if (square.charAt(1) === '8' || square.charAt(1)  === '7') {
+            color = "b";
+        }
+        else {
+            color = "w";
+        }
+
+        //paint squares
+        if (square==="a8" || square==="h8" || square==="a1" || square==="h1") {
+          var pathImage = "../images/tower"+color+".png";
+          paintSquare(document,square,true,pathImage);
+        }
+        else if (square==="b8" || square==="g8" || square==="b1" || square==="g1") {
+          var pathImage = "../images/knight"+color+".png";
+          paintSquare(document,square,true,pathImage);
+        }
+        else if (square==="c8" || square==="f8" || square==="c1" || square==="f1") {
+          var pathImage = "../images/bishop"+color+".png";
+          paintSquare(document,square,true,pathImage);
+        }        
+        else if (square==="d8" || square==="d1") {
+          var pathImage = "../images/queen"+color+".png";
+          paintSquare(document,square,true,pathImage);
+        }
+        else if (square==="e8" || square==="e1") {
+          var pathImage = "../images/king"+color+".png";
+          paintSquare(document,square,true,pathImage);
+        }     
+        else if (square.charAt(1) === "7" || square.charAt(1) === "2") {
+          var pathImage = "../images/pawn"+color+".png";
+          paintSquare(document,square,true,pathImage);
+        }        
+     
+        else {
+          paintSquare(document,square,false,null);
+        }
+    }
+}
+
+
