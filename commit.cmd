2aca058539cc15357abb49329fd6f3a7a4f2cc50
unknown <sebastianrevuelta@gmail.com>
2020-04-23 10:02:47 +0200

    TuningMovement

---
+++ commit.cmd
@@ -0,0 +1,4 @@
+cd C:\Users\srevuelta\eclipse-workspace\Chess
+git config --global user.email "sebastianrevuelta@gmail.com"
+git add .
+git commit -m %1
---
+++ diff.cmd
@@ -0,0 +1,2 @@
+cd C:\Users\srevuelta\eclipse-workspace\Chess
+git diff --name-only HEAD > diff.txt
