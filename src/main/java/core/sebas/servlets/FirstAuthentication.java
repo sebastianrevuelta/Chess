daf6c0aa6214c39f1539d500489193961b9abcee
Sebastián Revuelta <sebastian.revuelta@kiuwan.com>
2020-08-18 14:07:19 +0200

    new sha1 algorithm

--- src/main/java/core/sebas/servlets/FirstAuthentication.java
+++ src/main/java/core/sebas/servlets/FirstAuthentication.java
@@ -1,3 +1,38 @@
+package core.sebas.servlets;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class FirstAuthentication extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (Exception e) {}
+	}
+
 	@Override
 	protected void doPost(HttpServletRequest request,final HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
@@ -64,3 +99,5 @@
 			requestDispatcher.forward(request, response);
 		}
 	}
+	
+}
