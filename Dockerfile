9ce16f5a5713873a32b2eebba48ef1426e03eab1
Sebastian Revuelta Marchetti <T0245924@LAB-ES.SPACE.THALES>
2020-09-29 13:31:05 +0200

    create docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
+#it is so much better to use a official image
 FROM davidcaste/alpine-tomcat:tomcat8
 LABEL maintainer="sebastianrevuelta@gmail.com"
+RUN mkdir /home/sebas
+#create the user
+RUN addgroup -g 8877 chessgroup 
+RUN adduser -u 8877 -G chessgroup -h /home/sebas -D sebas
+WORKDIR /home/sebas
+#it is better to use COPY instead of ADD
 ADD chess.war /opt/tomcat/webapps/
+
+#is it possible to expose another ports?
 EXPOSE 8080
 CMD ["/opt/tomcat/bin/catalina.sh", "run"]
--- launch.cmd
+++ launch.cmd
@@ -4,5 +4,6 @@ docker container stop mysql-db
 docker container rm mysql-db
 docker container stop chess
 docker container rm chess
-docker run -d -p 8081:8080 --name chess chess-game 
-docker run -d -p 33060:3306 --name mysql-db  -e MYSQL_ROOT_PASSWORD=platas$79 --mount src=mysql-db-data,dst=/var/lib/mysql mysql
+docker run -d -p 8081:8080 --name chess chess-game
+docker run -d -p 33060:3306 --name mysql-db -e MYSQL_ROOT_PASSWORD=password --mount src=mysql-db-data,dst=/var/lib/mysql mysql
+docker exec -i mysql-db sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD"' < ./sql/chess_db.sql
