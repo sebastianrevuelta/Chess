d9fef4a0a56f0a11a99292d04764177bdd004d84
sebas <sebastianrevuelta@gmail.com>
2020-10-10 17:06:19 +0200

    ready to deploy in heroku

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: java $JAVA_OPTS -jar target/dependency/webapp-runner.jar --port 8081 chess.war
+
---
+++ start.sh
@@ -0,0 +1,10 @@
+ant war
+docker container stop mysql-db
+docker rm -f mysql-db
+docker volume prune --force
+docker volume create mysql-db-data
+docker run -d -p 33060:3306 --name mysql-db -e MYSQL_ROOT_PASSWORD=platas76 --mount src=mysql-db-data,dst=/var/lib/mysql mysql
+docker container stop chess
+docker container rm chess
+docker build -t chess-game .
+docker run -d -p 8081:8080 --name chess chess-game
