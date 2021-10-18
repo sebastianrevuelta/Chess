75ea279b4bf3f223f65bbc3cf26fa49563ca70ef
sebastianrevuelta <sebastianrevuelta@gmail.com>
2021-10-18 16:06:41 +0200

    add docker-compose

--- launch.sh
+++ launch.sh
@@ -1,8 +1,7 @@
 ## Chess app
-docker stop mysql_db
-docker rm mysql_db
+#docker stop mysql_db
+#docker rm mysql_db
 docker run -d -p 3306:3306 --name mysql_db -e MYSQL_ROOT_PASSWORD=q1w2e3r4 --mount src=mysql-db-data,dst=/var/lib/mysql mysql
 docker-compose up -d
-
 ## Review database content with mycli
 ## mycli mysql://root@localhost:3306/chess
