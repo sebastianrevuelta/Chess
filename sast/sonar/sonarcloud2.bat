f437af4e9297537a4cf36a1581ded34500c7966a
Sebastian Revuelta <sebastianrevuelta@gmail.com>
2024-02-09 10:56:56 +0100

    Update sonarcloud2.bat

--- sast/sonar/sonarcloud2.bat
+++ sast/sonar/sonarcloud2.bat
@@ -1,1 +1,1 @@
-mvn sonar:sonar -Dsonar.projectKey=sebastianrevuelta_Chess -Dsonar.organization=sebastianrevuelta-github -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=8fdf86930411847804b9d797dd1a4b7acdfd2ae5
+mvn sonar:sonar -Dsonar.projectKey=sebastianrevuelta_Chess -Dsonar.organization=sebastianrevuelta-github -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=%SONAR_KEY%
